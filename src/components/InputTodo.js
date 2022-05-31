e50fc4bd2a6ebeb17d203629d98f50a5a97c8a0b
Dinma-Faith <phaytep@gmail.com>
2022-05-31 10:02:07 +0100

    Work on react form and event handlers

---
+++ src/components/InputTodo.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react"
+
+class InputTodo extends Component {
+    state = {
+        fName: "",
+        lastName: "",
+      }
+
+  onChange = e => {
+    this.setState({
+  [e.target.name]: e.target.value,
+})
+  }
+
+//   Submit
+handleSubmit = e => {
+  e.preventDefault()
+  if (this.state.title.trim()) {
+    this.props.addTodoProps(this.state.title)
+    this.setState({
+      title: "",
+    })
+  } else {
+    alert("Please write item")
+  }
+}
+
+  render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add todo..."
+          value={this.state.title}
+          name="title"
+          onChange={this.onChange}
+        />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+export default InputTodo
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,34 +1,79 @@
 import React from "react"
+import { v4 as uuidv4 } from "uuid";
 import TodosList from "./TodosList";
 import Header from "./Header";
+import InputTodo from "./InputTodo";
 
 
 class TodoContainer extends React.Component {
     state = {
         todos: [
           {
-            id: 1,
+            id: uuidv4(),
             title: "Setup development environment",
             completed: true
           },
           {
-            id: 2,
+            id: uuidv4(),
             title: "Develop website and add content",
             completed: false
           },
           {
-            id: 3,
+            id: uuidv4(),
             title: "Deploy to live server",
             completed: false
           }
         ]
        };
-       
+
+       handleChange = id => {
+        this.setState(prevState => {
+          return {
+            todos: prevState.todos.map(todo => {
+              if (todo.id === id) {
+                return {
+                  ...todo,
+                  completed: !todo.completed,
+                }
+              }
+              return todo
+            }),
+          }
+        })
+      };
+
+      // Delete Button
+      delTodo = id => {
+        this.setState({
+          todos: [
+            ...this.state.todos.filter(todo => {
+              return todo.id !== id;
+            })
+          ]
+        });
+      };
+
+      // Submit Button
+      addTodoItem = title => {
+        const newTodo = {
+          id: 4,
+          title: title,
+          completed: false
+        };
+        this.setState({
+          todos: [...this.state.todos, newTodo]
+        });
+      };
+
        render() {
         return (
           <div>
             <Header />
-            <TodosList todos={this.state.todos} />
+            <InputTodo addTodoProps={this.addTodoItem} />
+            <TodosList todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+            />
           </div>
         );
       }
