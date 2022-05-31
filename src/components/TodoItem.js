bb29967e23d26e9b319f545366b81703cc765fbf
Dinma-Faith <phaytep@gmail.com>
2022-05-31 15:18:43 +0100

    Update ReadMe

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,70 +1,76 @@
-import React from "react"
-import styles from "./TodoItem.module.css"
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/state-in-constructor */
+
+import React from 'react';
+import styles from './TodoItem.module.css';
 
 class TodoItem extends React.Component {
   state = {
     editing: false,
-  }
+  };
 
   handleEditing = () => {
     this.setState({
       editing: true,
-    })
-  }
+    });
+  };
 
-  handleUpdatedDone = event => {
-    if (event.key === "Enter") {
-      this.setState({ editing: false })
+  handleUpdatedDone = (event) => {
+    if (event.key === 'Enter') {
+      this.setState({ editing: false });
     }
-  }
+  };
 
   componentWillUnmount() {
-    console.log("Cleaning up...")
+    console.log('Cleaning up...');
   }
 
   render() {
     const completedStyle = {
-      fontStyle: "italic",
-      color: "#595959",
+      fontStyle: 'italic',
+      color: '#595959',
       opacity: 0.4,
-      textDecoration: "line-through",
+      textDecoration: 'line-through',
+    };
+
+    const { completed, id, title } = this.props.todo;
+
+    const viewMode = {};
+    const editMode = {};
+
+    if (this.state.editing) {
+      viewMode.display = 'none';
+    } else {
+      editMode.display = 'none';
     }
-
-    const { completed, id, title } = this.props.todo
-
-    let viewMode = {}
-let editMode = {}
-
-if (this.state.editing) {
-  viewMode.display = "none"
-} else {
-  editMode.display = "none"
-}
 
     return (
       <li className={styles.item}>
-        <div onDoubleClick={this.handleEditing} style={viewMode}><input
-          type="checkbox"
-          className={styles.checkbox}
-          checked={completed}
-          onChange={() => this.props.handleChangeProps(id)}
-        />
-        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
-        <span style={completed ? completedStyle : null}>{title}</span></div>
+        <div onDoubleClick={this.handleEditing} style={viewMode}>
+          <input
+            type="checkbox"
+            className={styles.checkbox}
+            checked={completed}
+            onChange={() => this.props.handleChangeProps(id)}
+          />
+          <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+          <span style={completed ? completedStyle : null}>{title}</span>
+        </div>
         <input
           type="text"
           style={editMode}
           className={styles.textInput}
           value={title}
-          onChange={e => {
-            this.props.setUpdate(e.target.value, id)
+          onChange={(e) => {
+            this.props.setUpdate(e.target.value, id);
           }}
           onKeyDown={this.handleUpdatedDone}
 
         />
       </li>
-    )
-}
+    );
+  }
 }
 
-export default TodoItem
+export default TodoItem;
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,12 +1,17 @@
-import React from "react"
-import TodoItem from "./TodoItem";
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/state-in-constructor */
 
+import React from 'react';
+import TodoItem from './TodoItem';
+
+// eslint-disable-next-line react/prefer-stateless-function
 class TodosList extends React.Component {
   render() {
     return (
       <ul>
-        {this.props.todos.map(todo => (
-            <TodoItem
+        {this.props.todos.map((todo) => (
+          <TodoItem
             key={todo.id}
             todo={todo}
             handleChangeProps={this.props.handleChangeProps}
@@ -14,11 +19,10 @@ class TodosList extends React.Component {
             setUpdate={this.props.setUpdate}
           />
 
-            
         ))}
       </ul>
-    )
+    );
   }
 }
 
-export default TodosList
+export default TodosList;
